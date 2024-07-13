e29e5ead6024f4636e9744b5a539bc963302feaa
sachindk <sachindk52@gmail.com>
2024-07-13 23:40:51 +0530

    add Day1 of 30days JS challenge

---
+++ day1/script.js
@@ -0,0 +1,38 @@
+//Activity 1
+    //Task1
+var rollnum = 10;
+console.log(rollnum)
+
+    //Task2
+let name = "Amal";
+console.log(name);
+
+//Activity 2
+//Task3
+const value = true;
+console.log(value);
+
+//Task4
+
+let studentName = "arun";
+let roll = 21;
+const pi = 3.14;
+let subject = ["Maths", "Discret", 20];
+let flag = false;
+
+const bike = {model:"ApacheRTR", CC:"250"};
+
+console.log(typeof(bike));
+
+//Task5
+
+let person = "Rocky";
+console.log(person);
+person = "Rocky Bhai";
+console.log(person);
+
+//Task 6
+const no = 12;
+console.log(no);
+no = 56;
+console.log(no);
